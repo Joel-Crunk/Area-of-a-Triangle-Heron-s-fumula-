f8d745cd3aa011fe85fff6882ce6c4856e388174
Joel-Crunk <joel.crunk@outlook.com>
2019-06-19 15:41:11 -0400

    This commit will find the perimeter of a triangle

--- AreaOfATriangleHW/Program.cs
+++ AreaOfATriangleHW/Program.cs
@@ -11,27 +11,22 @@ namespace AreaOfATriangleHW
         static void Main(string[] args)
         {
             Console.Write("Enter the first side of the triangle: ");
-            int s1 = Int32.Parse(Console.ReadLine());
+            int side1 = Int32.Parse(Console.ReadLine());
 
             Console.Write("Enter the second side of the triangle: ");
-            int s2 = Int32.Parse(Console.ReadLine());
+            int side2 = Int32.Parse(Console.ReadLine());
 
             Console.Write("Enter the third side of the triangle: ");
-            int s3 = Int32.Parse(Console.ReadLine());
+            int side3 = Int32.Parse(Console.ReadLine());
 
-            double p = (s1 + s2 + s3) / 2.0;
+            double halfPerimeter = (side1 + side2 + side3) / 2.0;
             //2.0 is to make the division work as a double 
 
-            double val = p * (p - s1) * (p - s2) * (p - s3);
-           
-
+            double val = halfPerimeter * (halfPerimeter - side1) * (halfPerimeter - side2) * (halfPerimeter - side3);
             double answer = Math.Sqrt(val);
            
-
             Console.WriteLine($"The area of this triangle is {answer}.");
-
             Console.ReadLine();
-
         }
     }
 }
